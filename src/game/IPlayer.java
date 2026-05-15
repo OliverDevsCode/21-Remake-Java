ae268e6534d49e1cd28ab715f6a5e0948ebd9726
“Recursive-Li” <285008007+Recursive-Li@users.noreply.github.com>
2026-05-15 20:29:38 +0100

    finished player prototype

--- src/game/IPlayer.java
+++ src/game/IPlayer.java
@@ -3,7 +3,9 @@ package game;
 public interface IPlayer {
     public void resetHand();
     public void addCard(Card card);
-    @Override
-    String toString();
+    public void dealDamage(int health);
+    public int handSum();
+    public int getValue();
+    String toString(Card card);
 }
 
--- src/game/Player.java
+++ src/game/Player.java
@@ -6,9 +6,9 @@ public class Player implements IPlayer{
     int health;
     boolean stand;
 
-    public Player(){
+    public Player(int health){
         hand = new ArrayList<Card>();
-        health = 3;
+        this.health = health;
         stand = false;
     }
     public void resetHand(){
@@ -17,12 +17,57 @@ public class Player implements IPlayer{
     public void addCard(Card card){
         hand.add(card);
     }
+    public void dealDamage(int amount){
+        this.health -= amount;
+    }
+    public int getValue(CardValue value){
+        switch (value) {
+        case ACE:
+                return 1;
+            case TWO:
+                return 2;
+        case THREE:
+                return 3;
+        case FOUR:
+                return 4;
+        case FIVE:
+                return 5;
+        case SIX:
+                return 6;
+        case SEVEN:
+                return 7;
+        case EIGHT:
+                return 8;
+        case NINE:
+                return 9;
+        case TEN:
+                return 10;
+        case JACK:
+                return 10;
+        case QUEEN:
+                return 10;
+        case KING:
+                return 10;
+        default:
+            return 0;
+       }
+    }
+    public int handSum(){
+        int sum = 0;
+        for (Card card : hand) {
+            sum += getValue(card.value);
+        }
+        return sum;
+    }
+
     @Override
-    public String toString(){
+    public String toString(Card card){
         StringBuffer output = new StringBuffer();
         for(int i =0; i < hand.size();){
             output.append("{");
-            
+            output.append(card);
+            output.append("}");
         }
+        return output.toString();
     }
 }
