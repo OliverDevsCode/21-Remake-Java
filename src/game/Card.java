89790eb4f5c40cdff82b4d36fe3b4aaac81f7fe9
Your Name <186149954+OliverDevsCode@users.noreply.github.com>
2026-05-15 18:48:26 +0100

    optimised toString method (CARD)

--- src/game/Card.java
+++ src/game/Card.java
@@ -24,51 +24,11 @@ public class Card implements ICard{
     @Override
     public String toString(){
         StringBuffer output = new StringBuffer();
-       switch (value) {
-        case ACE:
-                output.append("ace");
-        break;
-        case TWO:
-                output.append("2");
-        break;
-        case THREE:
-                output.append("3");
-        break;
-        case FOUR:
-                output.append("4");
-        break;
-        case FIVE:
-                output.append("5");
-        break;
-        case SIX:
-                output.append("6");
-        break;
-        case SEVEN:
-                output.append("7");
-        break;
-        case EIGHT:
-                output.append("8");
-        break;
-        case NINE:
-                output.append("9");
-        break;
-        case TEN:
-                output.append("10");
-        break;
-        case JACK:
-                output.append("Jack");
-        break;
-        case QUEEN:
-                output.append("Queen");
-        break;
-        case KING:
-                output.append("King");
-        break;
-       
-        default:
-            output.append("NULL");
-            break;
-       }
+        for(CardValue value: CardValue.values()){
+            if(value == this.value){
+                output.append(value.name());
+            }
+        }
        return output.toString();
     }
 }
